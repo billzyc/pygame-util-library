9889bb8eaba07fbb3be1690d599442b5855842c0
Bill <billcshto@gmail.com>
2020-04-17 02:07:33 -0400

    update formatting

--- text_box_gui/test.py
+++ text_box_gui/test.py
@@ -2,17 +2,17 @@ import pygame
 from text_box import TextBox
 
 pygame.init()
-window = pygame.display.set_mode((800,800))
+window = pygame.display.set_mode((800, 800))
 
-testTextBox = TextBox(40, 100, 200, 50, 'this is a text')
+testTextBox = TextBox(40, 100, 200, 50, "this is a text")
 
 running = True
 while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
 
-  window.fill((54,54,54))
-  testTextBox.draw(window)
+    window.fill((54, 54, 54))
+    testTextBox.draw(window)
 
-  pygame.display.update()
+    pygame.display.update()
--- text_box_gui/text_box.py
+++ text_box_gui/text_box.py
@@ -1,19 +1,32 @@
 import pygame
 
-class TextBox:
-  def __init__(self, x, y ,width, height, content, fontSize = 20, bgColor = False, textColor = (0,0,0)):
-    self.x = x
-    self.y = y
-    self.width = width
-    self.height = height
-    self.image = pygame.Surface((width, height))
-    self.fontSize = fontSize
-    self.bgColor = bgColor
-    self.textColor = textColor
-    self.content = pygame.font.SysFont('arial', 15).render(content, 1, self.textColor)
 
-  def draw(self, window):
-    if(self.bgColor):
-      self.image.fill(self.bgColor)
-      window.blit(self.image, (self.x, self.y))
-    window.blit(self.content, (self.x, self.y))
+class TextBox:
+    def __init__(
+        self,
+        x,
+        y,
+        width,
+        height,
+        content,
+        fontSize=20,
+        bgColor=False,
+        textColor=(0, 0, 0),
+    ):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image = pygame.Surface((width, height))
+        self.fontSize = fontSize
+        self.bgColor = bgColor
+        self.textColor = textColor
+        self.content = pygame.font.SysFont("arial", 15).render(
+            content, 1, self.textColor
+        )
+
+    def draw(self, window):
+        if self.bgColor:
+            self.image.fill(self.bgColor)
+            window.blit(self.image, (self.x, self.y))
+        window.blit(self.content, (self.x, self.y))
